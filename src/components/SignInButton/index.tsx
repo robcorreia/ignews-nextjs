50165ff109bd87a8f7b55a768de340b397155444
Robson <robccorreia@gmail.com>
2021-08-05 16:32:54 -0300

    add auth  github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,25 @@
 import { FaGithub } from 'react-icons/fa'
-import { FiX} from 'react-icons/fi';
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession();
+    console.log(session);
 
-
-    return isUserLoggedIn ? (
-            <button type="button" className={styles.signInButton}>
-                <FaGithub color="#04d361"/>Robson Correia
-                <FiX color="#737380" className={styles.closeIcon} />
-            </button>
-    ) : (
+    return session ? (
         <button type="button" className={styles.signInButton}>
-            <FaGithub color="#eba417"/>Sign in with Github
+            <FaGithub color="#04d361" />{session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} onClick={() => signOut()} />
         </button>
+    ) : (
+        <button onClick={() => signIn('github')}
+            type="button" className={styles.signInButton}>
+            <FaGithub color="#eba417"
+            />Sign in with Github
+        </button>
+
     )
 }
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,7 +1,15 @@
 import Stripe from 'stripe';
 import { version } from '../../package.json'
 
-export const stripe = new Stripe( 
+//ESTRATEGIAS DE AUTENTICACAO USUARIO
+
+// JWT (Storage)
+// Next Auth (Login social, )
+// Codnito, Auth0
+
+
+
+export const stripe = new Stripe(
   process.env.STRIPE_API_KEY,
   {
     apiVersion: '2020-08-27',
